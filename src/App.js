bfdb2a3dfaf7401bbeea69fa0b998804757b6eac
Cristi Paun <ckrysty_an@yahoo.com>
2022-12-06 15:41:57 +0200

    A start at building a social media website

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/home/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
+
+// make general color and add it to everything
+// make darkmode color of ^ and replace it when swap to darkmode
---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,18 @@
+import "./rightbar.css";
+
+export default function Rightbar() {
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Who Asked</b> and <b>3 other friends</b> haave a birthday today
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+//1:14:31
